c48dad2fbf727ca527e8840407b90a39dacb5326
RoyalBlood <nzgoool1@gmail.com>
2024-09-05 17:04:42 +0400

    change model for critics

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
 import torch
 import torch.nn as nn
 import torch._dynamo
-from settings import ACTION_
+from settings import ACTION_, STATE_SIZE, SEED
+import numpy as np
 torch._dynamo.config.suppress_errors = True
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
     
 class Actor(nn.Module):
     def __init__(self, state_size, action_size):
@@ -43,32 +49,61 @@ class Actor(nn.Module):
         return torch.cat([v*ACTION_, w*ACTION_], dim=-1)
     
 class Critic1(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size= STATE_SIZE, action_size=ACTION_, seed= SEED, fcs1_units=400, fc2_units=300):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
         super(Critic1, self).__init__()
-        self.fc1 = nn.Linear(state_size + action_size, 400)
-        self.fc2 = nn.Linear(400, 300)
-        self.fc3 = nn.Linear(300, 1)
-        
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        x = torch.cat([state, action], dim=1)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.relu(self.fc2(x))
         return self.fc3(x)
     
-
 class Critic2(nn.Module):
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size= STATE_SIZE, action_size=ACTION_, seed= SEED, fcs1_units=450, fc2_units=350):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fcs1_units (int): Number of nodes in the first hidden layer
+            fc2_units (int): Number of nodes in the second hidden layer
+        """
         super(Critic2, self).__init__()
-        self.fc1 = nn.Linear(state_size + action_size, 500)
-        self.fc2 = nn.Linear(500, 512)
-        self.fc3 = nn.Linear(512, 500)
-        self.fc4 = nn.Linear(500, 1)
-        
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, 1)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
-        x = torch.cat([state, action], dim=1)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        return self.fc4(x)
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        xs = F.relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
