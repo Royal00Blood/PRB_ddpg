1ff5d4149e09a029181771790a5d3a97a8618e89
RoyalBlood <nzgoool1@gmail.com>
2024-09-05 16:48:45 +0400

    change model for actor

--- model.py
+++ model.py
@@ -3,48 +3,41 @@ import torch.nn as nn
 import torch._dynamo
 from settings import ACTION_
 torch._dynamo.config.suppress_errors = True
-
-# class Actor(nn.Module):
-#     def __init__(self, state_size, action_size):
-#         super(Actor, self).__init__()
-#         self.fc1 = nn.Linear(state_size, 400)
-#         self.fc2 = nn.Linear(400, 300)
-#         self.fc3 = nn.Linear(300, action_size)
-        
-#     def forward(self, state):
-#         x = torch.relu(self.fc1(state))
-#         x = torch.relu(self.fc2(x))
-#         return torch.tanh(self.fc3(x))*0.5
     
 class Actor(nn.Module):
     def __init__(self, state_size, action_size):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_size, 100)
+        self.fc1 = nn.Linear(state_size, 256)
+        self.fc2 = nn.Linear(256, 300)
         
-        self.fc2 = nn.Linear(100, 150)
+        self.fc3v = nn.Linear(300, 350)
+        self.fc3w = nn.Linear(300, 350)
         
-        self.fc3 = nn.Linear(150, 200)
-        self.fc4 = nn.Linear(200, 250)
-       
+        self.fc4v = nn.Linear(350, 400)
+        self.fc4w = nn.Linear(350, 400)
         
-        self.fc5v = nn.Linear(250, 200)
-        self.fc5w = nn.Linear(250, 200)
-        self.bn5 = nn.LayerNorm(200)
+        self.fc5v = nn.Linear(400, 512)
+        self.fc5w = nn.Linear(400, 512)
+        self.bn5 = nn.LayerNorm(512)
         
-        self.fc6v = nn.Linear(200, 1)
-        self.fc6w = nn.Linear(200, 1)
+        self.fc6v = nn.Linear(512, 1)
+        self.fc6w = nn.Linear(512, 1)
         
     def forward(self, state):
     
         x = torch.relu(self.fc1(state))
-        
         x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
-        x = torch.relu(self.fc4(x))
         
-        v = torch.relu(self.fc5v(x))
-        w = torch.relu(self.fc5w(x))
+        v = torch.relu(self.fc3v(x))
+        v = torch.relu(self.fc4v(v))
+        v = torch.relu(self.fc5v(v))
+        v = self.bn5(v)
         v = torch.tanh(self.fc6v(v))
+        
+        w = torch.relu(self.fc3w(x))
+        w = torch.relu(self.fc4w(w))
+        w = torch.relu(self.fc5w(w))
+        w = self.bn5(w)
         w = torch.tanh(self.fc6w(w))
         
         return torch.cat([v*ACTION_, w*ACTION_], dim=-1)
