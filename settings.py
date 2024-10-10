927ef7edea081df98df33e8bb993c48e3d9df8bc
Ivan <nazgoool1@gmail.com>
2024-10-10 14:53:27 +0400

    tau ->05

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ ALPHA = 0.7
 
 # Гиперпараметры
 GAMMA = 0.99   # 0.9 to 0.999
-TAU   = 0.01 # 0.001 to 0.01
+TAU   = 0.005 # 0.001 to 0.01
 
 LR_A = 0.0001 # 0.0001 to 0.001
 LR_C = 0.001 # 0.001 to 0.01
@@ -28,15 +28,15 @@ AREA_GENERATION = AREA_DEFEAT - S_G_TARG
 TIME = 0.1
 
 # Параметры обучения
-EPISODES = 6000
-EP_STEPS = 300
+EPISODES = 20000
+EP_STEPS = 350
 
 #  Параметры проверки
 TEST_EPISODES = 100
 TEST_EP_STEPS = 300
 
 # Параметры моделей
-SEED = 200
+SEED = 0
 
 # Кол нейронов в слоях
     #[ly1, ly2, ly3(v/w), ly4(v/w), ly5(v/w)] 
